87a952e9a27d0912012818dc7c89865f9dde3060
Jiaqi Yan <littlepretty881203@gmail.com>
2016-02-21 14:09:33 -0600

    Read in swf trace file
    Signed-off-by: Jiaqi Yan <littlepretty881203@gmail.com>

---
+++ test_read.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from simulator import BBSimulator
+from scheduler import BBScheduler, BBCpu, BBBurstBuffer, BBIo
+from trace_reader import BBTraceReader
+import logging
+
+
+def testTraceReader():
+    simulator = BBSimulator()
+
+    cpu = BBCpu(100, 10, 1)
+    bb = BBBurstBuffer(200, 10, 1)
+    io = BBIo(1, 1)
+
+    scheduler = BBScheduler(cpu, bb, io)
+    simulator.setScheduler(scheduler)
+
+    trace_reader = BBTraceReader('test.swf')
+    jobs = trace_reader.generateJobs()
+
+    simulator.simulate(jobs)
+
+
+if __name__ == '__main__':
+    # logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    testTraceReader()
---
+++ trace_reader.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import logging
+import numpy as np
+from job import BBJob, BBJobDemand
+
+
+class BBTraceReader(object):
+    """Read in swf trace file"""
+    def __init__(self, filename):
+        super(BBTraceReader, self).__init__()
+        self.trace = np.loadtxt(filename, comments=';')
+
+    def generateJob(self, row):
+        """convert one row of data to job object"""
+        job_id = row[0]
+        submit = row[1]
+        runtime = row[3]
+
+        num_core = row[4]
+        bb_in = row[18]
+        bb = row[19]
+        data_out = row[20]
+        demand = BBJobDemand(num_core, bb_in, bb, data_out)
+
+        job = BBJob(job_id, submit, demand, runtime)
+        return job
+
+    def generateJobs(self):
+        """return all jobs in trace file"""
+        jobs = []
+        for row in self.trace:
+            jobs.append(self.generateJob(row))
+        return jobs
